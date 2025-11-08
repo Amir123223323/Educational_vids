c5d388bdf9866e8cb3534d632f30e4105837f31e
Amir mohammad Akhlaghi <amirmohammad12669@gmail.com>
2025-11-08 22:26:19 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const links_communication_DR_mohammadi = [
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1631869935662",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1631939465716",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1632112368770",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1632546932163",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1633152960788",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1633325723706",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1633757828047",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1633929494438",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1634361460170",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1634534461909",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1634967122792",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1635139859713",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1635572280471",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1635744588392",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1636176807218",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1636349593799",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1636780787623",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1636953850324",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1637386152762",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1637558796966",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1637990683541",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1638163398618",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1638595429888",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1638768089617",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1639201145882",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1639372927615",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1639805208025",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1639806851957",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1639978582028",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1640409381812",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1640582988225",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/ea8b0e9ff9d881209873b4b77746d6b3e22f6e68-1642568749748"
+];
+const links_linearcontrol_systems_DR_Sharifi = [
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1631945368843",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1631947603937",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1632118147175",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1632553970703",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1633158545488",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1633331603688",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1633762370307",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1633935568737",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1634368150731",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1634540955282",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1634972892103",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1635145975520",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1635577998124",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1635750786629",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1636182503847",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1636355592607",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1636787149733",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1636959978618",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1637392130309",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1637564587726",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1637997169187",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1638169938602",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1638601710732",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1638774412037",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1639206716409",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1639379296711",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1639811377374",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1639984059216",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1640416096332",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1640588869186",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1641722521763",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/5103e5a197bd093fa74c601ab2a9b1c6d893bc6b-1643019652933"
+];
+const links_linearcontrol_systems_DR_Talebi = [
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1631946054659",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1631948789366",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1631948816781",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1632117828061",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1632553800120",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1633158514653",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1633331726636",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1633763415539",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1633936342120",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1634368354848",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1634541236991",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1634973013619",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1635145795300",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1635395412998",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1635578154418",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1635750827266",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1636183140357",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1636355265905",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1636787662359",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1636960533051",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1637209975099",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1637392758353",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1637565557077",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1637997468934",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1638170354382",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1638602178871",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1638774995623",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639028829778",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639206913216",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639379781985",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639629071525",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639812078247",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1639984599506",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1640416821697",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1640589107789",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1640928701352",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1641021293196",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1641028355564",
+    "https://blue.aut.ac.ir/playback/presentation/2.3/10809850c887d0c853cf9f2fcf80cc1d0d290f86-1641720014390"
+];
+const courses = ["communication_mohammadi","linear control systems DR Talebi","linear control systems DR sharifi"]
+const library = [links_communication_DR_mohammadi,links_linearcontrol_systems_DR_Talebi,links_linearcontrol_systems_DR_Sharifi];
+ function createlink(){
+    const course_select = document.getElementById('course');
+    let index = course_select.options[course_select.selectedIndex].value;
+    index = Number(index);
+    let path = library[index - 1];
+    const number = document.querySelector("#number");
+    const link = document.querySelector("#link");
+    number.placeholder = `1-${path.length}`
+    if (0 < number.value && number.value< path.length && number.value - Math.floor(number.value) === 0)
+        {
+            link.href = path[number.value - 1];
+            link.textContent = `${courses[index -1]} session ${number.value}`;
+        }
+        else{
+            link.textContent = `لطفا عد صحیح از 1 تا ${path.length} را انتخاب کنید`;
+            link.computedStyleMap.color = "red";
+            link.href = null;
+        } 
+                
+
+}           
